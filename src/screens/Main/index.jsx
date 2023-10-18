4efd5d7c87a08253c21d711f6058a537c87464a8
Geovana Rocha <133898564+gegekkjg@users.noreply.github.com>
2023-10-18 14:09:41 +0000

    tt

---
+++ src/screens/Main/index.jsx
@@ -0,0 +1,8 @@
+import { Box, Text } from "@gluestack-ui/themed";
+
+
+export default function Main(){
+    return <Box bg="$amber600">
+        <Text>Projeto Gluestack - Alterado </Text>
+    </Box>
+}
