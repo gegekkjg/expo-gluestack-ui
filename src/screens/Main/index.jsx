bce8ec0d0111ddaba3eae81ca8ecdde75cae3ba3
Geovana Rocha <133898564+gegekkjg@users.noreply.github.com>
2023-10-23 14:32:20 +0000

    gg

--- src/screens/Main/index.jsx
+++ src/screens/Main/index.jsx
@@ -1,8 +1,20 @@
-import { Box, Text } from "@gluestack-ui/themed";
+import { Box, Center, FormControl, FormControlLabel, FormControlLabelText, Heading, Input, InputField, Text } from "@gluestack-ui/themed";
 
 
 export default function Main(){
-    return <Box bg="$amber600">
-        <Text>Projeto Gluestack - Alterado </Text>
+    return <Box bg="$primary100" p="$2" h={200} w={300}>
+        <Center h={"$full"}>
+            <Heading>Peso Ideal</Heading>
+            <FormControl>
+                <FormControlLabel>
+                    <FormControlLabelText>
+                        Peso
+                    </FormControlLabelText>
+                </FormControlLabel>
+                <Input w={"$full"}>
+                    <InputField />
+                </Input>
+            </FormControl>
+        </Center>
     </Box>
 }
